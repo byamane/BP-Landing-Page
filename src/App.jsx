46d7298037b56b0c2d74c53536cf652fcaabc300
byamane <byamane21@gmail.com>
2022-06-14 19:08:32 -0500

    stub up sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,29 @@ import { useState } from 'react';
 // Components
 import Nav from './components/Nav';
 import Landing from './pages/Landing';
+import Signup from './components/Signup';
 
 function App() {
   
+  const [form, setForm] = useState({})
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
   return (
     <div className="App">
       <Nav />
+      <Signup 
+        form={form}
+        setForm={setForm}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
       <Landing />
     </div>
   )
