56896f9aaff7d5949f3d17beec26d66f038b4716
byamane <byamane21@gmail.com>
2022-06-15 19:37:46 -0500

    commit to Heroku

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -1,20 +1,26 @@
+import './Donate.css'
+
 const Donate = (props) => {
   return (
-    <>
+    <div id="donate-container">
       <h2>DONATE TO SUPPORT SAMANTHA'S CAMPAIGN</h2>
-      <button>
-        $25
-      </button>
-      <button>
-        Other
-      </button>
-      <button>
-        $50
-      </button>
-      <button>
-        $100
-      </button>
-    </>
+      <div className='donate-buttons'>
+        <button>
+          $25
+        </button>
+        <button>
+          Other
+        </button>
+      </div>
+      <div className='donate-buttons'>
+        <button>
+          $50
+        </button>
+        <button>
+          $100
+        </button>
+      </div>
+    </div>
   )
 }
 
